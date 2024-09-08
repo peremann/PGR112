70195baa0726ec11491e721e0fdaa76fb0509666
peremann <per.lauvas@kristiania.no>
2024-09-08 12:32:53 +0200

    Adjusting module 3 example.

--- src/eksempler/_03/Main.java
+++ src/eksempler/_03/Main.java
@@ -2,19 +2,7 @@ package eksempler._03;
 
 public class Main {
     public static void main(String[] args) {
-
-
         TheClass theClass = new TheClass();
         theClass.printRandomString();
-        theClass.strings[1] = "sdfdf";
-        theClass.strings[2] = "aaaaaa";
-        theClass.printRandomString();
-
-        String s1 = "hei";
-        String s2 = "hei";
-        boolean same = (s1==s2);
-        System.out.println("SAme:"+same);
-        System.out.println(s1==s2);
-
     }
 }
