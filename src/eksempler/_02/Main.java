0923e8bf384cccb02ee34ed191dfe5e79ebcee61
peremann <per.lauvas@kristiania.no>
2024-10-03 15:07:28 +0200

    Minor adjustment examples 2 - bicycle

--- src/eksempler/_02/Main.java
+++ src/eksempler/_02/Main.java
@@ -2,23 +2,21 @@ package eksempler._02;
 
 public class Main {
     public static void main(String[] args) {
-        Bicycle bicycle1 = new Bicycle();
-        bicycle1.gear = 1;
-        bicycle1.speed = 5;
-        bicycle1.isFast = true;
-        bicycle1.speedUp(1);
+        Bicycle bicycle = new Bicycle();
+        bicycle.gear = 1;
+        bicycle.speed = 5;
+        bicycle.isFast = true;
+        bicycle.speedUp(1);
         System.out.println("Look at my beautiful bicycle:");
-        bicycle1.printStates();
+        bicycle.printStates();
 
         Bicycle bicycle2 = new Bicycle();
         bicycle2.gear = 2;
         bicycle2.speed = 7;
         bicycle2.isFast = false;
         bicycle2.speedUp(10);
+        bicycle2.changeGear(3);
         System.out.println("Look at my beautiful bicycle2:");
         bicycle2.printStates();
-
-        bicycle2.speedUntil20();
-
     }
 }
